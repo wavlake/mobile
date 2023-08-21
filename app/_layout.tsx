e8202883627a609be0ab0b4f3d50ff15a4b085ba
Sam Samskies <samsamskies@gmail.com>
2023-08-21 16:33:38 -0500

    enable default ErrorBoundary

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,9 @@ import { Stack } from "expo-router";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { HeaderTitleLogo } from "../components";
 
+// Catch any errors thrown by the Layout component.
+export { ErrorBoundary } from "expo-router";
+
 export default function Layout() {
   return (
     <ThemeProvider value={DarkTheme}>
