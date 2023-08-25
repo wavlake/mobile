916ef7e8afb523b046d17148eaf38e798965013e
Sam Samskies <samsamskies@gmail.com>
2023-08-25 12:42:24 -0500

    display discover and top 40 screens in tab view

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabLayout() {
         }}
       >
         <Tabs.Screen
-          name="index"
+          name="(home)"
           options={{
             title: "Home",
             tabBarIcon: ({ color }) => <HomeIcon color={color} />,
