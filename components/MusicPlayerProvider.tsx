f6ac084f61e5f24b313cd0d535289ba15ecc2305
Sam Samskies <samsamskies@gmail.com>
2023-08-29 18:24:33 -0500

    refactor MusicPlayerProvider
    needed to make some changes for the full size music player

--- components/MusicPlayerProvider.tsx
+++ components/MusicPlayerProvider.tsx
@@ -16,16 +16,8 @@ export interface MusicPlayerItem {
   durationInMs: number;
 }
 
-interface Song {
-  artworkUrl: string;
-  title: string;
-  artist: string;
-  durationInMs: number;
-  sound: Audio.Sound;
-}
-
 interface MusicPlayerContextProps {
-  currentSong: Omit<Song, "sound"> | null;
+  currentSong: MusicPlayerItem | null;
   isPlaying: boolean;
   positionInMs: number;
   loadItem: (item: MusicPlayerItem) => Promise<void>;
@@ -39,35 +31,33 @@ const MusicPlayerContext = createContext<MusicPlayerContextProps | null>(null);
 
 export const MusicPlayerProvider = ({ children }: PropsWithChildren) => {
   const songQueue = useRef<MusicPlayerItem[]>([]);
-  const [currentSong, setCurrentSong] = useState<Song | null>(null);
+  const currentSound = useRef<Audio.Sound | null>(null);
+  const currentSongIndex = useRef(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [positionInMs, setPositionInMs] = useState<number>(0);
-  const loadItem = async ({
-    liveUrl,
-    artworkUrl,
-    title,
-    artist,
-    durationInMs,
-  }: MusicPlayerItem) => {
-    if (currentSong) {
-      await currentSong.sound.unloadAsync();
+  const [currentSong, setCurrentSong] = useState<MusicPlayerItem | null>(null);
+
+  const loadItem = async (item: MusicPlayerItem) => {
+    if (currentSound.current) {
+      await currentSound.current.unloadAsync();
     }
 
     const { sound } = await Audio.Sound.createAsync({
-      uri: liveUrl,
+      uri: item.liveUrl,
     });
 
+    currentSound.current = sound;
     await Audio.setAudioModeAsync({
       playsInSilentModeIOS: true,
       staysActiveInBackground: true,
     });
     sound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
     setIsPlaying(true);
-    setCurrentSong({ artworkUrl, title, artist, durationInMs, sound });
+    setCurrentSong(item);
     await sound.playAsync();
   };
   const loadItemList = async (itemList: MusicPlayerItem[]) => {
-    songQueue.current = itemList.slice(1);
+    songQueue.current = itemList;
 
     if (itemList[0]) {
       await loadItem(itemList[0]);
@@ -81,53 +71,49 @@ export const MusicPlayerProvider = ({ children }: PropsWithChildren) => {
     if (
       status.isLoaded &&
       status.didJustFinish &&
-      songQueue.current.length > 0
+      currentSongIndex.current < songQueue.current.length - 1
     ) {
-      await loadItem(songQueue.current.shift()!);
+      const nextSongIndex = currentSongIndex.current + 1;
+
+      await loadItem(songQueue.current[nextSongIndex]);
+      currentSongIndex.current = nextSongIndex;
     }
 
     if (
       status.isLoaded &&
       status.didJustFinish &&
-      songQueue.current.length === 0
+      currentSongIndex.current >= songQueue.current.length - 1
     ) {
-      await clear();
+      setIsPlaying(false);
+      await currentSound.current?.setPositionAsync(0);
     }
   };
   const play = async () => {
-    await currentSong?.sound.playAsync();
+    await currentSound.current?.playAsync();
     setIsPlaying(true);
   };
   const pause = async () => {
-    await currentSong?.sound.pauseAsync();
+    await currentSound.current?.pauseAsync();
     setIsPlaying(false);
   };
   const clear = async () => {
-    await currentSong?.sound.unloadAsync();
+    await currentSound.current?.unloadAsync();
     setCurrentSong(null);
+    currentSongIndex.current = 0;
     songQueue.current = [];
     setIsPlaying(false);
   };
 
   useEffect(() => {
     return () => {
-      if (currentSong) {
-        currentSong.sound.unloadAsync();
-      }
+      currentSound.current?.unloadAsync();
     };
   }, []);
 
   return (
     <MusicPlayerContext.Provider
       value={{
-        currentSong: currentSong
-          ? {
-              artworkUrl: currentSong.artworkUrl,
-              title: currentSong.title,
-              artist: currentSong.artist,
-              durationInMs: currentSong.durationInMs,
-            }
-          : null,
+        currentSong,
         isPlaying,
         positionInMs,
         loadItem,
