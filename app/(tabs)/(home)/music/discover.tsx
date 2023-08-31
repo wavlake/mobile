eab1947441d8f0aee72f92ad0e0c2e7c1945b9c7
Sam Samskies <samsamskies@gmail.com>
2023-08-30 21:28:50 -0500

    fix click to play on discover page

--- app/(tabs)/(home)/music/discover.tsx
+++ app/(tabs)/(home)/music/discover.tsx
@@ -5,48 +5,37 @@ import {
   TouchableOpacity,
   FlatList,
 } from "react-native";
-import {
-  Text,
-  SongArtwork,
-  useMusicPlayer,
-  MusicPlayerItem,
-} from "@/components";
+import { Text, SongArtwork, useMusicPlayer } from "@/components";
 import { useNewMusic } from "@/hooks";
+import { formatMusicItemForMusicPlayer } from "@/utils";
 
 export default function DiscoverPage() {
   const { data } = useNewMusic();
   const [songMetadataContainerWidth, setSongMetadataContainerWidth] =
     useState(0);
-  const { loadItem } = useMusicPlayer();
+  const { loadItemList } = useMusicPlayer();
 
   const handleSongMetadataContainerLayout = (event: LayoutChangeEvent) => {
     const { width } = event.nativeEvent.layout;
     setSongMetadataContainerWidth(width);
   };
-  const handleRowPress = async (item: MusicPlayerItem) => {
-    await loadItem(item);
+  const handleRowPress = async (index: number) => {
+    await loadItemList({
+      itemList: formatMusicItemForMusicPlayer(data),
+      startIndex: index,
+      playerTitle: "New music",
+    });
   };
 
   return (
     <View style={{ height: "100%", paddingTop: 16 }}>
       <FlatList
         data={data}
-        renderItem={({ item }) => {
-          const { liveUrl, artworkUrl, title, artist, duration, id } = item;
+        renderItem={({ item, index }) => {
+          const { artworkUrl, title, artist, id } = item;
 
           return (
-            <TouchableOpacity
-              key={id}
-              onPress={() =>
-                handleRowPress({
-                  liveUrl,
-                  artworkUrl,
-                  title,
-                  artist,
-                  durationInMs: duration * 1000,
-                })
-              }
-            >
+            <TouchableOpacity key={id} onPress={() => handleRowPress(index)}>
               <View
                 style={{
                   flexDirection: "row",
