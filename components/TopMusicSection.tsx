c093ed3307a7ef8aba8bc0568f20a1efd42f08d7
Sam Samskies <samsamskies@gmail.com>
2023-09-18 14:28:43 -0500

    update top music first row styling

--- components/TopMusicSection.tsx
+++ components/TopMusicSection.tsx
@@ -5,7 +5,6 @@ import { formatMusicItemForMusicPlayer, getTopMusic } from "@/utils";
 import { LayoutChangeEvent, View, TouchableOpacity } from "react-native";
 import { FireIcon } from "./FireIcon";
 import { Text } from "./Text";
-import { BadgeIcon } from "./BadgeIcon";
 import { useState } from "react";
 import { SongArtwork } from "./SongArtwork";
 import { useMusicPlayer } from "./MusicPlayerProvider";
@@ -47,8 +46,6 @@ export const TopMusicSection = () => {
       </View>
       {data.map((item, index) => {
         const { artworkUrl, title, artist, id } = item;
-        const isFirstRow = index === 0;
-        const artworkSize = isFirstRow ? 154 : 100;
 
         return (
           <TouchableOpacity key={id} onPress={() => handleRowPress(index)}>
@@ -58,45 +55,23 @@ export const TopMusicSection = () => {
                 marginBottom: 16,
               }}
             >
-              {!isFirstRow && (
-                <Text
-                  style={{
-                    fontSize: 36,
-                    width: 48,
-                    marginRight: 8,
-                    textAlign: "center",
-                    alignSelf: "center",
-                  }}
-                  bold
-                >
-                  {index + 1}
-                </Text>
-              )}
-              <SongArtwork size={artworkSize} url={artworkUrl} />
+              <Text
+                style={{
+                  fontSize: 36,
+                  width: 48,
+                  marginRight: 8,
+                  textAlign: "center",
+                  alignSelf: "center",
+                }}
+                bold
+              >
+                {index + 1}
+              </Text>
+              <SongArtwork size={100} url={artworkUrl} />
               <View
                 style={{ marginLeft: 10, flex: 1 }}
                 onLayout={handleSongMetadataContainerLayout}
               >
-                {isFirstRow && (
-                  <View style={{ marginVertical: 8 }}>
-                    <BadgeIcon
-                      fill={brandColors.mint.DEFAULT}
-                      width={48}
-                      height={48}
-                    />
-                    <Text
-                      style={{
-                        fontSize: 28,
-                        position: "absolute",
-                        top: 5,
-                        left: 18,
-                      }}
-                      bold
-                    >
-                      1
-                    </Text>
-                  </View>
-                )}
                 <Text
                   style={{ fontSize: 18, maxWidth: songMetadataContainerWidth }}
                   numberOfLines={2}
