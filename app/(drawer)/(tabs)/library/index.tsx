0b14ba4f7a4c9b12ef89b63238d8f3b064da4f98
Sam Samskies <samsamskies@gmail.com>
2023-10-02 12:20:33 -0500

    Revert "display coming soon message for library (#20)"
    This reverts commit 4cecf0bc852e6e09a21ded165609dc982a3b2b88.

--- app/(drawer)/(tabs)/library/index.tsx
+++ app/(drawer)/(tabs)/library/index.tsx
@@ -1,9 +1,51 @@
-import { Center, Text } from "@/components";
+import {
+  AlbumsIcon,
+  ArtistsIcon,
+  SongsIcon,
+  PlaylistsIcon,
+  Center,
+  LibraryMenu,
+  PillTabView,
+  Text,
+} from "@/components";
+import { brandColors } from "@/constants";
 
 export default function LibraryPage() {
   return (
-    <Center>
-      <Text>Library coming soon</Text>
-    </Center>
+    <PillTabView>
+      <PillTabView.Item style={{ width: "100%" }}>
+        <LibraryMenu>
+          <LibraryMenu.Item
+            title="Artists"
+            color={brandColors.pink.DEFAULT}
+            href="/library/music/artists"
+            Icon={ArtistsIcon}
+          />
+          <LibraryMenu.Item
+            title="Albums"
+            color={brandColors.purple.DEFAULT}
+            href="/library/music/albums"
+            Icon={AlbumsIcon}
+          />
+          <LibraryMenu.Item
+            title="Songs"
+            color={brandColors.mint.DEFAULT}
+            href="/library/music/songs"
+            Icon={SongsIcon}
+          />
+          <LibraryMenu.Item
+            title="Playlists"
+            color={brandColors.orange.DEFAULT}
+            href="/library/music/playlists"
+            Icon={PlaylistsIcon}
+          />
+        </LibraryMenu>
+      </PillTabView.Item>
+      <PillTabView.Item style={{ width: "100%" }}>
+        <Center>
+          <Text>Podcast library coming soon</Text>
+        </Center>
+      </PillTabView.Item>
+    </PillTabView>
   );
 }
