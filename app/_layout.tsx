8dc4977d8e40a309ae34981f1b314375ffae8a6b
Sam Samskies <samsamskies@gmail.com>
2023-10-11 11:40:29 -0700

    add ability to seek on lock screen (#48)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -64,6 +64,7 @@ export default function Layout() {
         Capability.Pause,
         Capability.SkipToNext,
         Capability.SkipToPrevious,
+        Capability.SeekTo,
       ],
     }).catch((error) => {
       console.log("error updating options", error);
