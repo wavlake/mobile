cefe0c71b61fc2571a4eb29ace777516033ac0c0
Josh Remaley <joshremaley@gmail.com>
2023-11-08 19:25:33 -0600

    add nwc to settings

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,11 +1,18 @@
 import { Button, Text, TextInput, WalletChooser } from "@/components";
 import { useLocalSearchParams } from "expo-router";
 import { Keyboard, TouchableWithoutFeedback, View } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth, useToast } from "@/hooks";
-import { cacheSettings } from "@/utils";
+import { cacheSettings, deleteNwcURI, getNwcURI, saveNwcURI } from "@/utils";
 import { Switch } from "@rneui/themed";
 import { brandColors } from "@/constants";
+import {
+  PlusCircleIcon,
+  TrashIcon,
+  EyeIcon,
+  EyeSlashIcon,
+} from "react-native-heroicons/solid";
+import { BarCodeScannedCallback, BarCodeScanner } from "expo-barcode-scanner";
 
 export default function SettingsPage() {
   const toast = useToast();
@@ -30,46 +37,140 @@ export default function SettingsPage() {
 
     toast.show("saved");
   };
+  const [showScanner, setShowScanner] = useState(false);
+  const onAddNWC = () => {
+    setShowScanner(true);
+  };
+
+  const onDeleteNWC = () => {
+    deleteNwcURI();
+    setNwcURI("");
+  };
+  const [scanned, setScanned] = useState(false);
+
+  const onBarCodeScanned: BarCodeScannedCallback = ({ data }) => {
+    if (scanned) return;
+    setScanned(true);
+    saveNwcURI(data);
+    setShowScanner(false);
+    setScanned(false);
+  };
+
+  const handleSaveNWC = () => {
+    saveNwcURI(newNwcURI);
+    setNwcURI("");
+    setShowScanner(false);
+  };
+
+  const [nwcURI, setNwcURI] = useState("");
+  const [newNwcURI, setNewNwcURI] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const storedNwcUri = await getNwcURI();
+
+      if (storedNwcUri) {
+        setNwcURI(storedNwcUri);
+      }
+    })();
+  }, [showScanner]);
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={{ padding: 24, gap: 24, alignItems: "center" }}>
-        <View style={{ marginBottom: 24, width: "100%" }}>
+      {showScanner ? (
+        <View
+          style={{
+            gap: 24,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <TextInput
-            label="Default zap amount"
-            value={defaultZapAmount}
-            keyboardType="numeric"
-            onChangeText={setDefaultZapAmount}
+            label="Nostr Wallet Connect pairing secret"
+            value={newNwcURI}
+            onChangeText={setNewNwcURI}
+            secureTextEntry={true}
           />
-          <WalletChooser
-            selectedWallet={defaultZapWallet}
-            onSelectedWalletChange={setDefaultZapWallet}
+          <Button onPress={handleSaveNWC}>Save</Button>
+          <Text>Scan a NWC QR code or paste one above</Text>
+          <BarCodeScanner
+            onBarCodeScanned={onBarCodeScanned}
+            style={{
+              width: "90%",
+              height: "70%",
+              borderColor: "white",
+              borderWidth: 1,
+            }}
           />
-          {pubkey && (
-            <View
-              style={{
-                marginTop: 24,
-                marginBottom: 4,
-                flexDirection: "row",
-              }}
-            >
-              <View style={{ flex: 1 }}>
-                <Text bold>Listening activity</Text>
-                <Text>
-                  Broadcast tracks you are listening to as a live status event
-                  to Nostr relays.
-                </Text>
-              </View>
-              <Switch
-                value={allowListeningActivity}
-                onValueChange={setAllowListeningActivity}
-                color={brandColors.pink.DEFAULT}
-              />
-            </View>
-          )}
         </View>
-        <Button onPress={handleSave}>Save</Button>
-      </View>
+      ) : (
+        <View style={{ padding: 24, gap: 24, alignItems: "center" }}>
+          <View style={{ marginBottom: 24, width: "100%" }}>
+            <TextInput
+              label="Default zap amount"
+              value={defaultZapAmount}
+              keyboardType="numeric"
+              onChangeText={setDefaultZapAmount}
+            />
+            <WalletChooser
+              selectedWallet={defaultZapWallet}
+              onSelectedWalletChange={setDefaultZapWallet}
+            />
+            {pubkey && (
+              <View>
+                <View
+                  style={{
+                    marginTop: 24,
+                    marginBottom: 4,
+                    flexDirection: "row",
+                  }}
+                >
+                  <View style={{ flex: 1 }}>
+                    <Text bold>Nostr Wallet Connect</Text>
+                    <Text>{nwcURI || "Add a NWC compatible wallet."}</Text>
+                  </View>
+                  {nwcURI ? (
+                    <TrashIcon
+                      onPress={onDeleteNWC}
+                      color={brandColors.pink.DEFAULT}
+                      height={40}
+                      width={40}
+                    />
+                  ) : (
+                    <PlusCircleIcon
+                      onPress={onAddNWC}
+                      color={brandColors.pink.DEFAULT}
+                      height={40}
+                      width={40}
+                    />
+                  )}
+                </View>
+                <View
+                  style={{
+                    marginTop: 24,
+                    marginBottom: 4,
+                    flexDirection: "row",
+                  }}
+                >
+                  <View style={{ flex: 1 }}>
+                    <Text bold>Listening activity</Text>
+                    <Text>
+                      Broadcast tracks you are listening to as a live status
+                      event to Nostr relays.
+                    </Text>
+                  </View>
+                  <Switch
+                    value={allowListeningActivity}
+                    onValueChange={setAllowListeningActivity}
+                    color={brandColors.pink.DEFAULT}
+                  />
+                </View>
+              </View>
+            )}
+          </View>
+          <Button onPress={handleSave}>Save</Button>
+        </View>
+      )}
     </TouchableWithoutFeedback>
   );
 }
