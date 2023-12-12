92f48df3b2be9422e3e016586aba1de866c1126b
Josh Remaley <joshremaley@gmail.com>
2023-12-12 14:13:24 -0600

    rm log

--- hooks/useZap.ts
+++ hooks/useZap.ts
@@ -129,7 +129,6 @@ export const useZap = ({
           walletPubkey: settings?.nwcPubkey,
           nwcRelay: settings?.nwcRelay,
         });
-        console.log({ result });
         if (error?.message) {
           toast.show(error.message);
         } else if (result?.preimage) {
