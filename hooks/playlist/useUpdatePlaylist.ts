edd10e05782ade797fbca4d29b77d3a54b6bf698
Josh Remaley <joshremaley@gmail.com>
2024-03-11 16:36:11 -0600

    add reorder hook

---
+++ hooks/playlist/useUpdatePlaylist.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { usePlaylistsQueryKey } from "./usePlaylistsQueryKey";
+import { reorderPlaylist } from "@/utils";
+
+export const useReorderPlaylist = () => {
+  const queryClient = useQueryClient();
+  const queryKey = usePlaylistsQueryKey();
+
+  return useMutation({
+    mutationFn: reorderPlaylist,
+    // When mutate is called:
+    onMutate: async (content) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({ queryKey });
+    },
+    // If the mutation fails,
+    // use the context returned from onMutate to roll back
+    onError: (error, _, context) => {
+      console.error(error);
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
+      queryClient.invalidateQueries(queryKey);
+    },
+  });
+};
