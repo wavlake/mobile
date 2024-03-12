3e27eda1ef5214a8f3f459b867f1107221dfcf2f
Josh Remaley <joshremaley@gmail.com>
2024-03-12 08:34:14 -0600

    remove repeated images

--- components/Mosaic.tsx
+++ components/Mosaic.tsx
@@ -4,70 +4,45 @@ import Svg, { Image as SvgImage } from "react-native-svg";
 import { LogoIcon } from "./LogoIcon";
 
 const imageSize = 30;
-const imagesPerRow = 2;
 
 const MosaicImage = ({ imageUrls }: { imageUrls: string[] }) => {
-  const images = imageUrls.slice(0, 4);
-  const renderMosaic = () => {
-    switch (images.length) {
-      case 1:
-        return (
-          <Image
-            source={{ uri: images[0] }}
-            style={{ width: imageSize * 2, height: imageSize * 2 }}
-          />
-        );
-      case 2:
-        return (
-          <Svg height={imageSize * 2} width={imageSize * 2}>
-            {[...images, ...images].map((image, index) => (
-              <SvgImage
-                key={index}
-                x={(index % 2) * imageSize}
-                y={Math.floor(index / 2) * imageSize}
-                width={imageSize}
-                height={imageSize}
-                href={{ uri: image }}
-              />
-            ))}
-          </Svg>
-        );
-      case 3:
-        return (
-          <Svg height={imageSize * 2} width={imageSize * 2}>
-            {[...images, images[0]].map((image, index) => (
-              <SvgImage
-                key={index}
-                x={(index % 2) * imageSize}
-                y={Math.floor(index / 2) * imageSize}
-                width={imageSize}
-                height={imageSize}
-                href={{ uri: image }}
-              />
-            ))}
-          </Svg>
-        );
-      case 4:
-        return (
-          <Svg height={imageSize * 2} width={imageSize * 2}>
-            {images.map((image, index) => (
-              <SvgImage
-                key={index}
-                x={(index % 2) * imageSize}
-                y={Math.floor(index / 2) * imageSize}
-                width={imageSize}
-                height={imageSize}
-                href={{ uri: image }}
-              />
-            ))}
-          </Svg>
-        );
-      default:
-        return <LogoIcon fill="white" width={60} height={60} />;
-    }
-  };
+  if (imageUrls.length === 0) {
+    return (
+      <View>
+        <LogoIcon fill="white" width={60} height={60} />;
+      </View>
+    );
+  }
 
-  return <View>{renderMosaic()}</View>;
+  if (imageUrls.length < 4) {
+    const [firstImage] = imageUrls;
+    return (
+      <View>
+        <Image
+          source={{ uri: firstImage }}
+          style={{ width: imageSize * 2, height: imageSize * 2 }}
+        />
+      </View>
+    );
+  }
+
+  const firstFourImages = imageUrls.slice(0, 4);
+  return (
+    <View>
+      <Svg height={imageSize * 2} width={imageSize * 2}>
+        {firstFourImages.map((image, index) => (
+          <SvgImage
+            key={index}
+            x={(index % 2) * imageSize}
+            y={Math.floor(index / 2) * imageSize}
+            width={imageSize}
+            height={imageSize}
+            href={{ uri: image }}
+          />
+        ))}
+      </Svg>
+    </View>
+  );
 };
 
 export default MosaicImage;
