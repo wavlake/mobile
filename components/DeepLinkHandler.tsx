9c345ca1fff4d3d121cd840650c400eb122cb373
joshr4 <joshremaley@gmail.com>
2024-03-18 19:46:14 -0500

    Configure deep linking handler (#124)
    * add android intent filter
    
    * rm old deeplink page
    
    * rm better url hook add deeplinkhandler
    
    * add android intents
    
    * add ios associatedDomains
    
    * add entitlements file
    
    * run npx expo prebuild --clean
    
    * rm os folders
    
    * rm logs
    
    * add deep link readme
    
    * rm readme line
    
    * add nav stack info to deep links
    
    * setup nwc to use new deeplinkhandler
    
    * add subdomain
    
    * edit app.json file to remove subdomain
    
    * add www and naked subdomains
    
    * Update app/nwcAdd.tsx
    
    Co-authored-by: Michael Rhee <77257032+blastshielddown@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Michael Rhee <77257032+blastshielddown@users.noreply.github.com>

---
+++ components/DeepLinkHandler.tsx
@@ -0,0 +1,85 @@
+import { useEffect } from "react";
+import * as Linking from "expo-linking";
+import { useRouter } from "expo-router";
+
+const ROUTE_MAPPING: Record<
+  string,
+  {
+    getPath: (id: string) => string;
+    includeBackButton: boolean;
+    history: string[];
+  }
+> = {
+  "playlist/": {
+    getPath: (id: string) => `/library/music/playlists/${id}`,
+    includeBackButton: true,
+    history: ["/library"],
+  },
+  "album/": {
+    getPath: (id: string) => `/album/${id}`,
+    includeBackButton: true,
+    history: ["/library"],
+  },
+  // used for artists
+  "": {
+    getPath: (name: string) => `/artist/${name}`,
+    includeBackButton: true,
+    history: ["/library"],
+  },
+  // TODO - build out mobile track page
+  // "track/": (id: string) => `/music/playlists/${id}`,
+};
+
+const DeepLinkHandler = () => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleDeepLink: Linking.URLListener = async (event) => {
+      const { path, queryParams } = Linking.parse(event.url);
+      // special NWC case
+      // for "Open in supported app" links
+      if (event.url.startsWith("nostr+walletconnect")) {
+        router.push({
+          pathname: "/nwc",
+          params: {
+            uri: event.url,
+          },
+        });
+        return;
+      }
+
+      // example: path = "/playlist/<playlist-ID>
+      if (!path) return;
+      for (const [
+        route,
+        { getPath, history, includeBackButton },
+      ] of Object.entries(ROUTE_MAPPING)) {
+        if (path.startsWith(route)) {
+          const id = path.split("/")[1];
+          const mobilePath = getPath(id);
+          // keeping temporarily to aid in debugging, can remove once deep links are stable
+          console.log("Handling universal link:", { path, id, mobilePath });
+
+          // this is needed to replicate the navigation path the user would have normally taken to reach the deep link
+          history.forEach((path) => router.push(path));
+          router.push({
+            pathname: mobilePath,
+            params: {
+              includeBackButton,
+            },
+          });
+          return;
+        }
+      }
+    };
+
+    const subscription = Linking.addEventListener("url", handleDeepLink);
+
+    return () => {
+      subscription.remove();
+    };
+  }, [router]);
+
+  return null;
+};
+
+export default DeepLinkHandler;
