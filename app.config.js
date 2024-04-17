10d7d082ec6507eb7272dbd36b36dfe9d4e131c0
Josh Remaley <joshremaley@gmail.com>
2024-04-17 17:00:47 -0500

    add comment

--- app.config.js
+++ app.config.js
@@ -1,8 +1,15 @@
-export default {
-  android: {
-    googleServicesFile: process.env.ANDROID_FILE_SECRET,
-  },
-  ios: {
-    googleServicesFile: process.env.IOS_FILE_SECRET,
-  },
+// these are references to firebase config files (that contain secrets) that live in the EAS build environment
+
+export default ({ config }) => {
+  return {
+    ...config,
+    android: {
+      ...config.android,
+      googleServicesFile: process.env.ANDROID_FILE_SECRET,
+    },
+    ios: {
+      ...config.ios,
+      googleServicesFile: process.env.IOS_FILE_SECRET,
+    },
+  };
 };
