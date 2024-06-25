222f54c176a584bd77c83a03a3d5b133bee90501
Josh Remaley <joshremaley@gmail.com>
2024-06-25 09:45:11 -0500

    add playlist tabs in library

--- app/(drawer)/(tabs)/library/music/playlists.tsx
+++ app/(drawer)/(tabs)/library/music/playlists.tsx
@@ -9,8 +9,65 @@ import {
   RefreshControl,
   View,
 } from "react-native";
+import { PillTabView } from "@/components";
+import { usePlaylists } from "@/hooks/playlist/usePlaylists";
 
-export default function PlaylistsPage() {
+const UserPlaylists = () => {
+  const basePath = useGetBasePathname();
+  const { height } = useMiniMusicPlayer();
+  const { data: playlists = [], isLoading, refetch } = usePlaylists();
+  const router = useRouter();
+  const handleRowPress = (playlist: { id: string; title: string }) => {
+    router.push({
+      pathname: `${basePath}/playlist/${playlist.id}`,
+      params: {
+        headerTitle: playlist.title,
+        includeBackButton: true,
+      },
+    });
+  };
+
+  if (isLoading) {
+    return (
+      <Center>
+        <ActivityIndicator />
+      </Center>
+    );
+  }
+
+  return (
+    <View style={{ height: "100%", paddingTop: 16, paddingHorizontal: 4 }}>
+      <FlatList
+        contentContainerStyle={{ flexGrow: 1 }}
+        data={playlists}
+        refreshControl={
+          <RefreshControl refreshing={isLoading} onRefresh={refetch} />
+        }
+        renderItem={({ item, index }) => {
+          const isLastRow = index === playlists.length - 1;
+          const onPress = () => handleRowPress(item);
+          return (
+            <PlaylistRow
+              playlist={item}
+              onPress={onPress}
+              isLastRow={isLastRow}
+              height={height}
+            />
+          );
+        }}
+        keyExtractor={(item) => item.id}
+        scrollEnabled
+        ListEmptyComponent={
+          <Center>
+            <Text>No playlists in your library yet.</Text>
+          </Center>
+        }
+      />
+    </View>
+  );
+};
+
+const NonUserPlaylists = () => {
   const basePath = useGetBasePathname();
   const { height } = useMiniMusicPlayer();
   const { data: playlists = [], isLoading, refetch } = useLibraryPlaylists();
@@ -62,5 +119,18 @@ export default function PlaylistsPage() {
         }
       />
     </View>
+  );
+};
+
+export default function PlaylistsPage() {
+  return (
+    <PillTabView tabNames={["Yours", "Others"]}>
+      <PillTabView.Item style={{ width: "100%" }}>
+        <UserPlaylists />
+      </PillTabView.Item>
+      <PillTabView.Item style={{ width: "100%" }}>
+        <NonUserPlaylists />
+      </PillTabView.Item>
+    </PillTabView>
   );
 }
