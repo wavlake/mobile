64785ebfadbdbc3856634be2b39bcd19a093ed0f
Josh Remaley <joshremaley@gmail.com>
2024-08-29 11:55:36 -0500

    bump build number

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@
 
 import { ExpoConfig, ConfigContext } from "expo/config";
 
-export const BUILD_NUM = 31;
+export const BUILD_NUM = 32;
 export const VERSION = "1.1.0";
 
 export default ({ config }: ConfigContext): ExpoConfig => {
