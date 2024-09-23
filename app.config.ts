4207f8fa542827b34354f07355f855b394337a95
Josh Remaley <joshremaley@gmail.com>
2024-09-23 19:47:47 +0200

    add android expo camera permission

--- app.config.ts
+++ app.config.ts
@@ -78,6 +78,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         "expo-camera",
         {
           cameraPermission: "Allow $(PRODUCT_NAME) to access your camera",
+          recordAudioAndroid: false,
         },
       ],
       [
