c070b2d3a148a6843a47f31026d56ab596e8f3a2
Josh Remaley <joshremaley@gmail.com>
2024-09-23 22:00:15 +0200

    have repeat button cycle through queue and track repeat modes

--- components/FullSizeMusicPlayer/RepeatButton.tsx
+++ components/FullSizeMusicPlayer/RepeatButton.tsx
@@ -1,27 +1,46 @@
 import { useTheme } from "@react-navigation/native";
 import { PressableIcon } from "../PressableIcon";
-import { Entypo } from "@expo/vector-icons";
 import { brandColors } from "@/constants";
 import { useMusicPlayer } from "../MusicPlayerProvider";
+import { RepeatMode } from "react-native-track-player";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import React from "react";
 
 interface RepeatButtonProps {
   size?: number;
 }
 
+const repeatIconMap: Record<RepeatMode, keyof typeof MaterialIcons.glyphMap> = {
+  [RepeatMode.Off]: "repeat",
+  [RepeatMode.Queue]: "repeat",
+  [RepeatMode.Track]: "repeat-one",
+};
+
 export const RepeatButton = ({ size = 24 }: RepeatButtonProps) => {
   const { colors } = useTheme();
-  const { toggleRepeatTrack, repeatMode } = useMusicPlayer();
+  const { toggleRepeatQueue, toggleRepeatTrack, repeatMode } = useMusicPlayer();
+
   const onPress = () => {
-    toggleRepeatTrack();
+    switch (repeatMode) {
+      case RepeatMode.Off:
+        toggleRepeatQueue();
+        break;
+      case RepeatMode.Queue:
+        toggleRepeatTrack();
+        break;
+      case RepeatMode.Track:
+        toggleRepeatQueue(); // This will set it to Off
+        break;
+    }
   };
+
+  const iconName = repeatIconMap[repeatMode];
+  const iconColor =
+    repeatMode === RepeatMode.Off ? colors.text : brandColors.pink.DEFAULT;
 
   return (
     <PressableIcon onPress={onPress}>
-      <Entypo
-        name="loop"
-        size={size}
-        color={repeatMode ? brandColors.pink.DEFAULT : colors.text}
-      />
+      <MaterialIcons name={iconName} size={size} color={iconColor} />
     </PressableIcon>
   );
 };
