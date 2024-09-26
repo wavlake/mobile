7e625e859ecffcf67aa41a3cf09d88fb303aaba8
Josh Remaley <joshremaley@gmail.com>
2024-09-26 23:34:57 +0200

    add now playing comment animation

--- components/FullSizeMusicPlayer/NowPlayingCommentSection.tsx
+++ components/FullSizeMusicPlayer/NowPlayingCommentSection.tsx
@@ -1,13 +1,14 @@
+import React, { useEffect, useRef, useState } from "react";
 import { useContentComments } from "@/hooks/useContentComments";
 import { CommentRow, Text } from "@/components";
 import { brandColors } from "@/constants";
-import { ActivityIndicator, Pressable, View } from "react-native";
+import { ActivityIndicator, Pressable, View, Animated } from "react-native";
 import { BottomSheet } from "@rneui/base";
-import { useEffect, useRef, useState } from "react";
 import { CommentList } from "../Comments/CommentList";
 import { ArrowTopRightOnSquareIcon } from "react-native-heroicons/solid";
 
 const COMMENT_ROTATION_INTERVAL = 5000;
+
 export const NowPlayingCommentSection = ({
   contentId,
 }: {
@@ -17,6 +18,8 @@ export const NowPlayingCommentSection = ({
   const [displayedComment, setDisplayedComment] = useState<string>();
   const [isExpanded, setIsExpanded] = useState(false);
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const slideAnim = useRef(new Animated.Value(0)).current;
+
   useEffect(() => {
     if (intervalRef.current) {
       clearInterval(intervalRef.current);
@@ -42,53 +45,69 @@ export const NowPlayingCommentSection = ({
     };
   }, [commentsData, contentId]);
 
+  const showCommentPreview = !isFetching && displayedComment;
+
+  useEffect(() => {
+    Animated.timing(slideAnim, {
+      toValue: showCommentPreview ? 0 : 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  }, [showCommentPreview, slideAnim]);
+
+  const slideInterpolate = slideAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 200], // Adjust this value based on your component's height
+  });
+
   return (
-    <Pressable
-      style={{
-        backgroundColor: brandColors.black.DEFAULT,
-        padding: 16,
-        borderRadius: 8,
-        marginBottom: 20,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        gap: 8,
-        minHeight: 120,
-      }}
-      onPress={() => {
-        setIsExpanded(true);
-      }}
-    >
-      <View
+    <>
+      <Animated.View
         style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
+          transform: [{ translateY: slideInterpolate }],
+          opacity: slideAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 0],
+          }),
         }}
       >
-        <Text style={{ fontSize: 18 }} bold>
-          Latest Messages
-        </Text>
-        <ArrowTopRightOnSquareIcon
-          color={brandColors.beige.dark}
-          height={20}
-          width={20}
-        />
-      </View>
-
-      {isFetching ? (
-        <View
-          style={{
-            paddingTop: 10,
-          }}
-        >
-          <ActivityIndicator />
-        </View>
-      ) : (
-        displayedComment && (
-          <CommentRow commentId={displayedComment} isPressable={false} />
-        )
-      )}
+        {showCommentPreview && (
+          <Pressable
+            style={{
+              backgroundColor: brandColors.black.DEFAULT,
+              padding: 16,
+              borderRadius: 8,
+              marginBottom: 20,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              gap: 8,
+              minHeight: 120,
+            }}
+            onPress={() => {
+              setIsExpanded(true);
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ fontSize: 18 }} bold>
+                Latest Messages
+              </Text>
+              <ArrowTopRightOnSquareIcon
+                color={brandColors.beige.dark}
+                height={20}
+                width={20}
+              />
+            </View>
+            <CommentRow commentId={displayedComment} isPressable={false} />
+          </Pressable>
+        )}
+      </Animated.View>
       <BottomSheet
         onBackdropPress={() => setIsExpanded(false)}
         containerStyle={{
@@ -114,6 +133,6 @@ export const NowPlayingCommentSection = ({
           showReplyLinks={false}
         />
       </BottomSheet>
-    </Pressable>
+    </>
   );
 };
