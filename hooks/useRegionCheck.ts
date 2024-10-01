216924435f9d24ffc92a98cec401478c1161c1a2
Josh Remaley <joshremaley@gmail.com>
2024-10-01 22:05:13 +0200

    add region check hook

---
+++ hooks/useRegionCheck.ts
@@ -0,0 +1,22 @@
+import { useUser } from "@/components";
+import { checkIPRegion } from "@/utils";
+import { useQuery } from "@tanstack/react-query";
+
+// checks request IP against region block list
+export const useRegionCheck = ({ enabled = true }) => {
+  const { user } = useUser();
+  return useQuery(
+    ["regionCheck", user?.uid],
+    async () => {
+      await checkIPRegion();
+      // this will only return if the request is status 200
+      // api returns 200 for success and 403 for rejection
+      // any other status code will be treated as a rejection
+      return true;
+    },
+    {
+      retry: false,
+      enabled: enabled,
+    },
+  );
+};
