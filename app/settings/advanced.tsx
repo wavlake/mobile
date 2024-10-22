109731ba9fe8e283e7b70506821c0d256b426098
Josh Remaley <joshremaley@gmail.com>
2024-10-22 12:02:56 -0500

    refactor advanced settings page

--- app/settings/advanced.tsx
+++ app/settings/advanced.tsx
@@ -7,15 +7,10 @@ import {
   ScrollView,
   Alert,
   TouchableOpacity,
+  StyleSheet,
 } from "react-native";
-import { useEffect, useState } from "react";
-import { useAuth, useToast } from "@/hooks";
-import {
-  WalletKey,
-  cacheSettings,
-  deleteNwcSecret,
-  payInvoiceCommand,
-} from "@/utils";
+import { useAuth, useToast, useSettingsManager } from "@/hooks";
+import { Settings, payInvoiceCommand } from "@/utils";
 import { useTheme } from "@react-navigation/native";
 import { Switch } from "@rneui/themed";
 import { brandColors } from "@/constants";
@@ -24,342 +19,267 @@ import {
   PlusCircleIcon,
   TrashIcon,
 } from "react-native-heroicons/solid";
-import { useQueryClient } from "@tanstack/react-query";
-import { useSettings } from "@/hooks/useSettings";
-import { useSettingsQueryKey } from "@/hooks/useSettingsQueryKey";
+
+const SettingsSwitch = ({
+  value,
+  onValueChange,
+  disabled,
+  description,
+  title,
+}: {
+  value: boolean;
+  onValueChange: (value: boolean) => void;
+  disabled?: boolean;
+  description: string;
+  title: string;
+}) => {
+  const { colors } = useTheme();
+
+  return (
+    <View style={styles.settingRow}>
+      <View style={styles.settingText}>
+        <Text bold>{title}</Text>
+        <Text>{description}</Text>
+      </View>
+      <Switch
+        value={value}
+        onValueChange={onValueChange}
+        disabled={disabled}
+        color={brandColors.pink.DEFAULT}
+        trackColor={{
+          false: colors.border,
+          true: brandColors.pink.DEFAULT,
+        }}
+        thumbColor={colors.text}
+      />
+    </View>
+  );
+};
 
 function getDomainFromWebSocket(wsAddress?: string) {
   if (!wsAddress) return "NWC";
   // Use a regular expression to match the domain
   const match = wsAddress.match(/^wss?:\/\/([^/:]+)/i);
-
   // Return the matched domain or null if no match
   return match ? match[1] : "NWC";
 }
 
-export default function AdvancedSettingsPage() {
-  const toast = useToast();
+const NWCSettings = ({
+  settings,
+  onUpdateSettings,
+}: {
+  settings: Settings;
+  onUpdateSettings: (settings: Partial<Settings>) => Promise<void>;
+}) => {
   const router = useRouter();
-  const { pubkey, userIsLoggedIn } = useAuth();
-  const { colors } = useTheme();
+  const nwcCantPayInvoices =
+    !!settings?.nwcRelay && !settings?.nwcCommands?.includes(payInvoiceCommand);
 
-  const { data: settings } = useSettings();
-
-  const [isFocusedOnZapAmount, setIsFocusedOnZapAmount] = useState(false);
-  const [defaultZapWallet, setDefaultZapWallet] = useState<WalletKey>(
-    settings?.defaultZapWallet ?? "default",
-  );
-  const [allowListeningActivity, setAllowListeningActivity] = useState(
-    settings?.allowListeningActivity ?? false,
-  );
-  const [enableNWC, setEnableNWC] = useState(settings?.enableNWC ?? false);
-
-  const [oneTapZap, setOneTapZap] = useState(settings?.oneTapZap ?? false);
-  const [publishKind1, setPublishKind1] = useState(
-    settings?.publishKind1 ?? false,
-  );
-
-  const queryClient = useQueryClient();
-  const settingsKey = useSettingsQueryKey();
-
-  const handleSave = async () => {
-    toast.clearAll();
-    Keyboard.dismiss();
-    await cacheSettings(
-      {
-        defaultZapWallet,
-        allowListeningActivity,
-        enableNWC,
-        oneTapZap,
-        publishKind1,
-      },
-      pubkey,
-    );
-    queryClient.invalidateQueries(settingsKey);
-    toast.show("saved");
-  };
-  const onAddNWC = () => {
-    router.push({
-      pathname: "/nwcScanner",
-    });
-  };
-
-  const onDeleteNWC = async () => {
+  const handleDeleteNWC = () => {
     Alert.alert(
       "Are you sure you want to delete this connection?",
       `You will no longer be able to send zaps with your ${getDomainFromWebSocket(
         settings?.nwcRelay,
       )} wallet. You will need to re-connect to use it again.`,
       [
-        {
-          text: "Cancel",
-          style: "cancel",
-        },
+        { text: "Cancel", style: "cancel" },
         {
           text: "Delete Connection",
           style: "destructive",
           onPress: async () => {
-            pubkey && deleteNwcSecret(pubkey);
-            await cacheSettings(
-              {
-                nwcRelay: undefined,
-                nwcCommands: [],
-                nwcPubkey: undefined,
-                nwcLud16: undefined,
-                enableNWC: false,
-              },
-              pubkey,
-            );
-            queryClient.invalidateQueries(settingsKey);
+            await onUpdateSettings({
+              nwcRelay: undefined,
+              nwcCommands: [],
+              nwcPubkey: undefined,
+              nwcLud16: undefined,
+              enableNWC: false,
+            });
           },
         },
       ],
     );
   };
 
-  // autosave settings on change
-  useEffect(() => {
-    if (!settings || isFocusedOnZapAmount) return;
-    if (
-      defaultZapWallet !== settings.defaultZapWallet ||
-      allowListeningActivity !== settings.allowListeningActivity ||
-      enableNWC !== settings.enableNWC ||
-      oneTapZap !== settings.oneTapZap ||
-      publishKind1 !== settings.publishKind1
-    ) {
-      handleSave();
+  if (!settings) return null;
+
+  return (
+    <View style={styles.nwcContainer}>
+      <View style={styles.nwcHeader}>
+        <View style={styles.nwcInfo}>
+          <Text bold>Nostr Wallet Connect (NWC)</Text>
+          <View style={styles.nwcStatus}>
+            {settings.nwcRelay && (
+              <CheckCircleIcon color={brandColors.mint.DEFAULT} />
+            )}
+            <Text>{settings.nwcRelay || "Add a NWC compatible wallet."}</Text>
+          </View>
+        </View>
+        {settings.nwcRelay ? (
+          <TrashIcon
+            onPress={handleDeleteNWC}
+            color={brandColors.orange.DEFAULT}
+            height={40}
+            width={40}
+            hitSlop={styles.iconHitSlop}
+          />
+        ) : (
+          <PlusCircleIcon
+            onPress={() => router.push({ pathname: "/nwcScanner" })}
+            color={brandColors.pink.DEFAULT}
+            height={40}
+            width={40}
+            hitSlop={styles.iconHitSlop}
+          />
+        )}
+      </View>
+      {nwcCantPayInvoices && (
+        <Text style={styles.errorText}>
+          It looks like this wallet cannot pay invoices, please try another
+          wallet connection.
+        </Text>
+      )}
+      {settings.nwcRelay && (
+        <SettingsSwitch
+          value={settings.enableNWC || false}
+          onValueChange={(value) => onUpdateSettings({ enableNWC: value })}
+          disabled={nwcCantPayInvoices}
+          title="Enable NWC Wallet"
+          description={
+            settings.enableNWC
+              ? "Disable to use a different wallet."
+              : "Enable to use NWC wallet."
+          }
+        />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
+    gap: 24,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    width: "100%",
+    textAlign: "left",
+  },
+  settingRow: {
+    flexDirection: "row",
+    width: "100%",
+  },
+  settingText: {
+    flex: 1,
+  },
+  nwcContainer: {
+    width: "100%",
+    gap: 16,
+  },
+  nwcHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  nwcInfo: {
+    flex: 1,
+  },
+  nwcStatus: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  errorText: {
+    color: brandColors.orange.DEFAULT,
+  },
+  iconHitSlop: {
+    top: 10,
+    bottom: 10,
+    left: 10,
+    right: 10,
+  },
+});
+
+export default function AdvancedSettingsPage() {
+  const toast = useToast();
+  const router = useRouter();
+  const { pubkey, userIsLoggedIn } = useAuth();
+  const { settings, updateSettings } = useSettingsManager(pubkey);
+
+  if (!settings) return null;
+
+  const handleSettingsUpdate = async (newSettings: Partial<Settings>) => {
+    try {
+      toast.clearAll();
+      Keyboard.dismiss();
+      await updateSettings(newSettings);
+      toast.show("saved");
+    } catch (error) {
+      toast.show("Failed to save settings");
+      console.error("Failed to save settings:", error);
     }
-  }, [
-    isFocusedOnZapAmount,
-    defaultZapWallet,
-    allowListeningActivity,
-    enableNWC,
-    oneTapZap,
-    publishKind1,
-  ]);
+  };
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <ScrollView
-        contentContainerStyle={{
-          padding: 24,
-          alignItems: "center",
-          display: "flex",
-          flexDirection: "column",
-          gap: 24,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 20,
-            width: "100%",
-            textAlign: "left",
-          }}
-        >
-          Wallet
-        </Text>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.sectionTitle}>Wallet</Text>
         <WalletChooser
-          selectedWallet={defaultZapWallet}
-          onSelectedWalletChange={setDefaultZapWallet}
+          selectedWallet={settings.defaultZapWallet ?? "default"}
+          onSelectedWalletChange={(wallet) =>
+            handleSettingsUpdate({ defaultZapWallet: wallet })
+          }
         />
         {userIsLoggedIn && (
-          <NWCSettings
-            setEnableNWC={setEnableNWC}
-            onDeleteNWC={onDeleteNWC}
-            onAddNWC={onAddNWC}
-          />
-        )}
-        {userIsLoggedIn && (
-          <View
-            style={{
-              flexDirection: "row",
-            }}
-          >
-            <View style={{ flex: 1 }}>
-              <Text bold>Listening activity</Text>
-              <Text>
-                Broadcast tracks you are listening to as a live status event to
-                Nostr relays.
-              </Text>
-            </View>
-            <Switch
-              value={allowListeningActivity}
-              onValueChange={setAllowListeningActivity}
-              color={brandColors.pink.DEFAULT}
-              trackColor={{
-                false: colors.border,
-                true: brandColors.pink.DEFAULT,
-              }}
-              thumbColor={colors.text}
+          <>
+            <NWCSettings
+              settings={settings}
+              onUpdateSettings={handleSettingsUpdate}
             />
-          </View>
-        )}
-        <View
-          style={{
-            flexDirection: "row",
-          }}
-        >
-          <View style={{ flex: 1 }}>
-            <Text bold>One tap zaps</Text>
-            <Text>
-              Change the default behavior of the zap button to one tap zap your
-              default amount. Long press to open the comment form.
-            </Text>
-          </View>
-          <Switch
-            value={oneTapZap}
-            onValueChange={setOneTapZap}
-            color={brandColors.pink.DEFAULT}
-            trackColor={{
-              false: colors.border,
-              true: brandColors.pink.DEFAULT,
-            }}
-            thumbColor={colors.text}
-          />
-        </View>
-        {userIsLoggedIn && (
-          <Text
-            style={{
-              fontSize: 20,
-              width: "100%",
-              textAlign: "left",
-            }}
-          >
-            Nostr
-          </Text>
-        )}
-        {userIsLoggedIn && (
-          <View
-            style={{
-              flexDirection: "row",
-            }}
-          >
-            <View style={{ flex: 1 }}>
-              <Text bold>Publish comments to nostr</Text>
-              <Text>
-                Publish comments to your nostr feed. These comments will show up
-                in other nostr clients as kind 1 events.
-              </Text>
-            </View>
-            <Switch
-              value={publishKind1}
-              onValueChange={setPublishKind1}
-              color={brandColors.pink.DEFAULT}
-              trackColor={{
-                false: colors.border,
-                true: brandColors.pink.DEFAULT,
-              }}
-              thumbColor={colors.text}
+            <SettingsSwitch
+              value={settings.allowListeningActivity ?? false}
+              onValueChange={(value) =>
+                handleSettingsUpdate({ allowListeningActivity: value })
+              }
+              title="Listening activity"
+              description="Broadcast tracks you are listening to as a live status event to Nostr relays."
             />
-          </View>
+          </>
         )}
+        <SettingsSwitch
+          value={settings.oneTapZap ?? false}
+          onValueChange={(value) => handleSettingsUpdate({ oneTapZap: value })}
+          title="One tap zaps"
+          description="Change the default behavior of the zap button to one tap zap your default amount. Long press to open the comment form."
+        />
         {userIsLoggedIn && (
-          <TouchableOpacity
-            hitSlop={20}
-            onPress={() => router.push({ pathname: "/settings/backup-nsec" })}
-          >
-            <View style={{ flex: 1 }}>
-              <Text bold>Export or update your nostr secret key</Text>
-              <Text>
-                Tap here to view your account secret key and export it to a safe
-                place.
-              </Text>
-            </View>
-          </TouchableOpacity>
+          <>
+            <Text style={styles.sectionTitle}>Nostr</Text>
+            <SettingsSwitch
+              value={settings.publishKind1 ?? false}
+              onValueChange={(value) =>
+                handleSettingsUpdate({ publishKind1: value })
+              }
+              title="Publish comments to nostr"
+              description="Publish comments to your nostr feed. These comments will show up in other nostr clients as kind 1 events."
+            />
+            <TouchableOpacity
+              hitSlop={20}
+              onPress={() => router.push({ pathname: "/settings/backup-nsec" })}
+            >
+              <View style={styles.settingText}>
+                <Text bold>Export or update your nostr secret key</Text>
+                <Text>
+                  Tap here to view your account secret key and export it to a
+                  safe place.
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </>
         )}
       </ScrollView>
     </TouchableWithoutFeedback>
   );
 }
-
-const NWCSettings = ({
-  setEnableNWC,
-  onDeleteNWC,
-  onAddNWC,
-}: {
-  setEnableNWC: (enable: boolean) => void;
-  onDeleteNWC: () => void;
-  onAddNWC: () => void;
-}) => {
-  const { data: settings } = useSettings();
-
-  const { colors } = useTheme();
-  const nwcCantPayInvoices =
-    !!settings?.nwcRelay && !settings?.nwcCommands.includes(payInvoiceCommand);
-
-  return (
-    <>
-      <View
-        style={{
-          flexDirection: "row",
-        }}
-      >
-        <View style={{ flex: 1 }}>
-          <Text bold>Nostr Wallet Connect (NWC)</Text>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 4,
-            }}
-          >
-            {settings?.nwcRelay && (
-              <CheckCircleIcon color={brandColors.mint.DEFAULT} />
-            )}
-            <Text>{settings?.nwcRelay || "Add a NWC compatible wallet."}</Text>
-          </View>
-        </View>
-        {settings?.nwcRelay ? (
-          <TrashIcon
-            onPress={onDeleteNWC}
-            color={brandColors.orange.DEFAULT}
-            height={40}
-            width={40}
-            hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
-          />
-        ) : (
-          <PlusCircleIcon
-            onPress={onAddNWC}
-            color={brandColors.pink.DEFAULT}
-            height={40}
-            width={40}
-            hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
-          />
-        )}
-      </View>
-      {nwcCantPayInvoices && (
-        <Text>
-          It looks like this wallet cannot pay invoices, please try another
-          wallet connection.
-        </Text>
-      )}
-      {settings?.nwcRelay && (
-        <View
-          style={{
-            flexDirection: "row",
-          }}
-        >
-          <View style={{ flex: 1 }}>
-            <Text bold>Enable NWC Wallet</Text>
-            <Text>
-              {settings?.enableNWC
-                ? "Disable to use a different wallet."
-                : "Enable to use NWC wallet."}
-            </Text>
-          </View>
-          <Switch
-            value={settings?.enableNWC}
-            onValueChange={setEnableNWC}
-            disabled={nwcCantPayInvoices}
-            color={brandColors.pink.DEFAULT}
-            trackColor={{
-              false: colors.border,
-              true: brandColors.pink.DEFAULT,
-            }}
-            thumbColor={colors.text}
-          />
-        </View>
-      )}
-    </>
-  );
-};
