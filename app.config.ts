2852764bc5783e560f21b32abbbe3acd58b71918
Josh Remaley <joshremaley@gmail.com>
2024-10-28 10:22:17 -0500

    bump build

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@
 
 import { ExpoConfig, ConfigContext } from "expo/config";
 
-export const BUILD_NUM = 60;
+export const BUILD_NUM = 61;
 export const VERSION = "1.1.0";
 export const getUserAgent = (modelName: string = "mobile") =>
   `Wavlake/${VERSION} ${modelName}/${BUILD_NUM} https://wavlake.com`;
