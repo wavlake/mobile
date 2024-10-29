14a08ab6f7eadb34c6f51c2d836c450cd506da34
Josh Remaley <joshremaley@gmail.com>
2024-10-29 12:01:55 -0500

    always load home page api data

--- hooks/useHomePage.ts
+++ hooks/useHomePage.ts
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { getHomePage, HomePageData } from "@/utils";
 import { useUser } from "@/components";
+import { useAuth } from "./useAuth";
 
 export const useHomePage = () => {
-  const { user, initializingAuth } = useUser();
+  const { user } = useUser();
+  const { pubkey } = useAuth();
 
   return useQuery<HomePageData>({
-    queryKey: ["homePage", !!user],
+    queryKey: ["homePage", user, pubkey],
     queryFn: getHomePage,
-    enabled: !initializingAuth,
   });
 };
