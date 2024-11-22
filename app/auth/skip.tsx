eddc19a2665cc4925014c3a011967f930d081a4b
Josh Remaley <joshremaley@gmail.com>
2024-11-22 15:17:40 -0500

    add scroll view to skip page

--- app/auth/skip.tsx
+++ app/auth/skip.tsx
@@ -2,6 +2,7 @@ import { Button, Center, Text } from "@/components";
 import { View } from "react-native";
 import { useRouter } from "expo-router";
 import { setSkipLogin } from "@/utils";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Skip() {
   const router = useRouter();
@@ -12,17 +13,19 @@ export default function Skip() {
   };
 
   return (
-    <Center>
-      <View style={{ paddingHorizontal: 36, paddingBottom: 120 }}>
-        <Text style={{ fontSize: 18 }}>
-          We recommend you set up an account to get the most out of the Wavlake
-          app, but you can use Wavlake completely anonymously if you wish. You
-          will miss out on some great features like having your own custom
-          library and special access for fans, but you can always set up your
-          profile later if you wish.
-        </Text>
-      </View>
-      <Button onPress={handleLoginAnonymously}>Just let me in</Button>
-    </Center>
+    <ScrollView>
+      <Center>
+        <View style={{ paddingHorizontal: 36, paddingBottom: 120 }}>
+          <Text style={{ fontSize: 18 }}>
+            We recommend you set up an account to get the most out of the
+            Wavlake app, but you can use Wavlake completely anonymously if you
+            wish. You will miss out on some great features like having your own
+            custom library and special access for fans, but you can always set
+            up your profile later if you wish.
+          </Text>
+        </View>
+        <Button onPress={handleLoginAnonymously}>Just let me in</Button>
+      </Center>
+    </ScrollView>
   );
 }
