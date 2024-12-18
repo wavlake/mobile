95d47c71977633a8ead80cf240e8ede1b4bb2721
Josh Remaley <joshremaley@gmail.com>
2024-12-18 12:43:13 -0600

    add replace option

--- hooks/useGoToAlbumPage.ts
+++ hooks/useGoToAlbumPage.ts
@@ -1,14 +1,19 @@
 import { useGetBasePathname } from "./useGetBasePathname";
 import { useRouter } from "expo-router";
 
-export const useGoToAlbumPage = () => {
+export const useGoToAlbumPage = ({
+  replace = false,
+}: {
+  replace?: boolean;
+}) => {
   const router = useRouter();
   const basePathname = useGetBasePathname();
 
   return (albumId: string, albumName: string) => {
-    return router.push({
+    const nextPage = {
       pathname: `${basePathname}/album/[albumId]`,
       params: { albumId, headerTitle: albumName, includeBackButton: "true" },
-    });
+    };
+    return replace ? router.replace(nextPage) : router.push(nextPage);
   };
 };
