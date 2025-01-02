cb40dce27105ec4071417f90264e8589d67f17e5
joshr4 <joshremaley@gmail.com>
2025-01-02 10:43:55 -0600

    Update Welcome pop up (#201)
    * bump build
    
    * tweak listen to earn welcome popup

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@
 
 import { ExpoConfig, ConfigContext } from "expo/config";
 
-export const BUILD_NUM = 85;
-export const VERSION = "1.1.0";
+export const BUILD_NUM = 3;
+export const VERSION = "1.1.1";
 export const getUserAgent = (modelName: string = "mobile") =>
   `Wavlake/${VERSION} ${modelName}/${BUILD_NUM} https://wavlake.com`;
 export default ({ config }: ConfigContext): ExpoConfig => {
