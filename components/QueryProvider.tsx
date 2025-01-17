1bd3474b27d5ecc0533240d5bd6dd20ff3cf0cc8
Josh Remaley <joshremaley@gmail.com>
2025-01-17 13:08:03 -0800

    use constants for query config times

--- components/QueryProvider.tsx
+++ components/QueryProvider.tsx
@@ -1,17 +1,18 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { QueryClient } from "@tanstack/react-query";
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
-import {
-  PersistQueryClientProvider,
-  persistQueryClient,
-} from "@tanstack/react-query-persist-client";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { PropsWithChildren } from "react";
+
+const FIVE_MINS = 1000 * 60 * 5; // 5 minutes
+const TWENTY_FOUR_HOURS = 1000 * 60 * 60 * 24; // 24 hours
+
 // Create the query client
-export const queryClient = new QueryClient({
+const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      gcTime: 1000 * 60 * 60 * 24, // Cache data for 24 hours
-      staleTime: 1000 * 60 * 5, // Data remains fresh for 5 minutes
+      gcTime: TWENTY_FOUR_HOURS, // Cache data for 24 hours
+      staleTime: FIVE_MINS, // Data remains fresh for 5 minutes
     },
   },
 });
@@ -21,20 +22,13 @@ const asyncStoragePersister = createAsyncStoragePersister({
   storage: AsyncStorage,
 });
 
-// Persist the query client
-persistQueryClient({
-  queryClient,
-  persister: asyncStoragePersister,
-  maxAge: 1000 * 60 * 60 * 24, // Maximum age of 24 hours
-});
-
 // Export the PersistQueryClientProvider for use in the app
 export const QueryProvider: React.FC<PropsWithChildren> = ({ children }) => (
   <PersistQueryClientProvider
     client={queryClient}
     persistOptions={{
       persister: asyncStoragePersister,
-      maxAge: 1000 * 60 * 60 * 24, // Set max age explicitly in persistOptions
+      maxAge: TWENTY_FOUR_HOURS,
     }}
   >
     {children}
