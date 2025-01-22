e23e17bd3fd1ed19bdb9c64e3ed26608e32d5508
Josh Remaley <joshremaley@gmail.com>
2025-01-22 12:23:10 -0800

    use amber for signing

--- utils/signing.ts
+++ utils/signing.ts
@@ -1,8 +1,20 @@
 import { EventTemplate, finalizeEvent, generateSecretKey } from "nostr-tools";
 import { hexToBytes } from "@noble/hashes/utils";
 import { getSeckey } from "./secureStorage";
+import { getIsLoggedInWithAmber } from "./cache";
+import { signEventWithAmber } from "@/hooks";
 
 export const signEvent = async (eventTemplate: EventTemplate) => {
+  const loggedInWithAmber = await getIsLoggedInWithAmber();
+
+  if (loggedInWithAmber) {
+    return signEventWithAmber(eventTemplate);
+  }
+
+  return signEventWithStoredKey(eventTemplate);
+};
+
+const signEventWithStoredKey = async (eventTemplate: EventTemplate) => {
   const loggedInUserSeckey = await getSeckey();
   const anonSeckey = generateSecretKey();
 
