c9d60bdedd0dea4528ab47c62c474730e59093bd
Josh Remaley <joshremaley@gmail.com>
2025-01-22 20:47:29 -0800

    update amber logic

--- hooks/useAmberSigner.ts
+++ hooks/useAmberSigner.ts
@@ -1,68 +1,82 @@
-import { getIsLoggedInWithAmber, setIsLoggedInWithAmber } from "@/utils";
-import { useQuery } from "@tanstack/react-query";
+import { setAmberPubkey } from "@/utils";
 import * as IntentLauncher from "expo-intent-launcher";
-import { EventTemplate } from "nostr-tools";
+import { Event, EventTemplate, nip19 } from "nostr-tools";
 import { Platform } from "react-native";
-
+import { checkInstalledApps } from "expo-check-installed-apps";
 interface AmberSignerResponse {
-  signature?: string;
-  package?: string;
-  id?: string;
   event?: string;
+  id?: string;
+  package?: string;
+  result?: string;
+  signature?: string;
 }
 
+const AMBER_PKG = "com.greenart7c3.nostrsigner";
+const SCHEME = "nostrsigner";
+
 export const signEventWithAmber = async (event: EventTemplate) => {
-  const res = await IntentLauncher.startActivityAsync(
+  const eventJson = JSON.stringify(event);
+  const result = await IntentLauncher.startActivityAsync(
     "android.intent.action.VIEW",
     {
-      data: "nostrsigner:",
-      packageName: "com.greenart7c3.nostrsigner",
-      type: "sign_event",
+      // Include the content in the URI as specified in the Amber docs
+      data: `${SCHEME}:${eventJson}`,
+      packageName: AMBER_PKG,
+      // Add these flags as mentioned in the Amber docs for signing multiple events
+      // flags: 0x14000000, // FLAG_ACTIVITY_SINGLE_TOP | FLAG_ACTIVITY_CLEAR_TOP
       extra: {
-        event: JSON.stringify(event),
+        type: "sign_event",
       },
     },
   );
-  console.log("Amber response, sign_event", res);
-  return res;
+  const response = result.extra as AmberSignerResponse;
+  if (!response.event) {
+    console.error("No event returned from signer", response);
+    return null;
+  }
+
+  const signedEvent: Event = JSON.parse(response.event);
+  return signedEvent;
 };
 
 export const useAmberSigner = () => {
-  const { data: isLoggedInWithAmber, refetch: refetchAmberStatus } = useQuery({
-    queryKey: ["isLoggedInWithAmber"],
-    queryFn: getIsLoggedInWithAmber,
-    staleTime: Infinity,
-  });
-
   const loginWithAmber = async () => {
     if (Platform.OS !== "android") return false;
 
     try {
-      // Request public key from Amber
+      const permissions = [
+        { type: "sign_event" },
+        { type: "nip04_encrypt" },
+        { type: "nip04_decrypt" },
+      ];
+
       const result = await IntentLauncher.startActivityAsync(
         "android.intent.action.VIEW",
         {
-          data: "nostrsigner:",
-          packageName: "com.greenart7c3.nostrsigner",
-          type: "get_public_key",
+          data: `${SCHEME}:`,
+          packageName: AMBER_PKG,
           extra: {
-            permissions: JSON.stringify([
-              { type: "sign_event" },
-              { type: "nip04_encrypt" },
-              { type: "nip04_decrypt" },
-            ]),
+            type: "get_public_key",
+            permissions: JSON.stringify(permissions),
           },
         },
       );
-      console.log("Amber response, get_public_key", result);
+
       if (result.resultCode !== IntentLauncher.ResultCode.Success) {
         return false;
       }
 
       const response = result.extra as AmberSignerResponse;
       if (!response.signature) return false;
-      await setIsLoggedInWithAmber(true);
-      refetchAmberStatus();
+
+      const { type, data } = nip19.decode(response.signature);
+      if (type === "npub") {
+        await setAmberPubkey(data);
+      } else {
+        console.error("Invalid Amber pubkey type:", type);
+        return false;
+      }
+
       return true;
     } catch (error) {
       console.error("Amber login error:", error);
@@ -70,73 +84,47 @@ export const useAmberSigner = () => {
     }
   };
 
-  const checkAmberInstalled = async (): Promise<boolean> => {
-    if (Platform.OS !== "android") return false;
-
-    try {
-      const result = await IntentLauncher.startActivityAsync(
-        "android.intent.action.VIEW",
-        {
-          data: "nostrsigner:",
-          flags: 0,
-        },
-      );
-      console.log("Amber response, check_installed", result);
-      return result.resultCode === IntentLauncher.ResultCode.Success;
-    } catch {
-      return false;
-    }
-  };
-
-  const getAmberPubkey = async () => {
-    if (Platform.OS !== "android") return "";
-
-    try {
-      const result = await IntentLauncher.startActivityAsync(
-        "android.intent.action.VIEW",
-        {
-          data: "nostrsigner:",
-          packageName: "com.greenart7c3.nostrsigner",
-          type: "get_public_key",
-        },
-      );
-
-      if (result.resultCode !== IntentLauncher.ResultCode.Success) {
-        return "";
-      }
-
-      const response = result.extra as AmberSignerResponse;
-      if (!response.id) return "";
-
-      return response.id;
-    } catch (error) {
-      console.error("Amber pubkey error:", error);
-      return "";
-    }
-  };
-
   const logoutFromAmber = async () => {
-    if (Platform.OS !== "android") return;
-
     try {
-      await IntentLauncher.startActivityAsync("android.intent.action.VIEW", {
-        data: "nostrsigner:",
-        packageName: "com.greenart7c3.nostrsigner",
-        type: "logout",
+      if (Platform.OS !== "android") return;
+
+      // clear the app's stored pubkey
+      await setAmberPubkey("");
+
+      // the user will be redirected to amber to logout
+      IntentLauncher.startActivityAsync("android.intent.action.VIEW", {
+        data: `${SCHEME}:`,
+        packageName: AMBER_PKG,
+        extra: {
+          type: "logout",
+        },
       });
 
-      await setIsLoggedInWithAmber(false);
-      refetchAmberStatus();
+      return true;
     } catch (error) {
       console.error("Amber logout error:", error);
+    }
+  };
+
+  const isAmberInstalled = async () => {
+    try {
+      const packageNames: string[] =
+        Platform.select({
+          android: [AMBER_PKG], // Use package name of android apps
+          ios: [`${SCHEME}:`], // unused, currently no ios signer app
+        }) || [];
+
+      const result = await checkInstalledApps(packageNames);
+      return result[AMBER_PKG] ?? false;
+    } catch (error) {
+      console.error("Error checking installed apps:", error);
+      return false;
     }
   };
 
   return {
     loginWithAmber,
     logoutFromAmber,
-    checkAmberInstalled,
-    getAmberPubkey,
-    isLoggedInWithAmber,
+    isAmberInstalled,
   };
 };
