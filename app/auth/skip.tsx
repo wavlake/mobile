806eed5168903a09c7e3558002c9f896152f071e
Josh Remaley <joshremaley@gmail.com>
2025-01-22 18:59:31 -0800

    center skip page content

--- app/auth/skip.tsx
+++ app/auth/skip.tsx
@@ -1,4 +1,4 @@
-import { Button, Center, Text } from "@/components";
+import { Button, Text } from "@/components";
 import { View, ScrollView } from "react-native";
 import { useRouter } from "expo-router";
 import { setSkipLogin } from "@/utils";
@@ -12,19 +12,23 @@ export default function Skip() {
   };
 
   return (
-    <ScrollView>
-      <Center>
-        <View style={{ paddingHorizontal: 36, paddingBottom: 120 }}>
-          <Text style={{ fontSize: 18 }}>
-            We recommend you set up an account to get the most out of the
-            Wavlake app, but you can use Wavlake completely anonymously if you
-            wish. You will miss out on some great features like having your own
-            custom library and special access for fans, but you can always set
-            up your profile later if you wish.
-          </Text>
-        </View>
-        <Button onPress={handleLoginAnonymously}>Just let me in</Button>
-      </Center>
+    <ScrollView
+      contentContainerStyle={{
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View style={{ paddingHorizontal: 36, paddingBottom: 120 }}>
+        <Text style={{ fontSize: 18 }}>
+          We recommend you set up an account to get the most out of the Wavlake
+          app, but you can use Wavlake completely anonymously if you wish. You
+          will miss out on some great features like having your own custom
+          library and special access for fans, but you can always set up your
+          profile later if you wish.
+        </Text>
+      </View>
+      <Button onPress={handleLoginAnonymously}>Just let me in</Button>
     </ScrollView>
   );
 }
