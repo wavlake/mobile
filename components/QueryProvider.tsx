9974ea43ae8c123e21c14fb073df17d4d556c3f3
Josh Remaley <joshremaley@gmail.com>
2025-02-12 08:41:52 -1000

    update queryprovider defaults, persist for infinity

--- components/QueryProvider.tsx
+++ components/QueryProvider.tsx
@@ -4,31 +4,25 @@ import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persi
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { PropsWithChildren } from "react";
 
-const FIVE_MINS = 1000 * 60 * 5; // 5 minutes
-const TWENTY_FOUR_HOURS = 1000 * 60 * 60 * 24; // 24 hours
-
-// Create the query client
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      gcTime: TWENTY_FOUR_HOURS, // Cache data for 24 hours
-      staleTime: FIVE_MINS, // Data remains fresh for 5 minutes
+      // gcTime: 1000 * 60 * 60 * 24, // Cache data for 24 hours
+      // staleTime: 1000 * 60 * 5, // Data remains fresh for 5 minutes
     },
   },
 });
 
-// Configure the AsyncStorage persister
 const asyncStoragePersister = createAsyncStoragePersister({
   storage: AsyncStorage,
 });
 
-// Export the PersistQueryClientProvider for use in the app
 export const QueryProvider: React.FC<PropsWithChildren> = ({ children }) => (
   <PersistQueryClientProvider
     client={queryClient}
     persistOptions={{
       persister: asyncStoragePersister,
-      maxAge: TWENTY_FOUR_HOURS * 14, // Persist data for 14 days
+      maxAge: Infinity,
     }}
   >
     {children}
