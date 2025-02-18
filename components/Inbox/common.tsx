acf2eab39156af746e914710f02c8f107ebc3c36
Josh Remaley <joshremaley@gmail.com>
2025-02-17 18:55:25 -1000

    Update item separator background color for improved visibility

--- components/Inbox/common.tsx
+++ components/Inbox/common.tsx
@@ -24,7 +24,7 @@ export const ItemSeparator = () => (
   <View
     style={{
       height: 1,
-      backgroundColor: "rgba(255, 255, 255, 0.2)",
+      backgroundColor: "rgba(255, 255, 255, 0.3)",
       marginVertical: 0,
     }}
   />
