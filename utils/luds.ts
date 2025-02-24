ae937374538f3f097678a1166a908d889b3a6159
Josh Remaley <joshremaley@gmail.com>
2025-02-24 13:34:45 -1000

    Fix LUD16 regex, improve validation

--- utils/luds.ts
+++ utils/luds.ts
@@ -12,8 +12,8 @@ interface LNURLPayResponse {
 // Get the initial LNURL endpoint from lud16 address
 export const getLNURLEndpointFromLUD16 = (lud16: string): string => {
   // Validate basic format, e.g. user@wavlake.com
-  const lud16Regex =
-    /^[a-z0-9-_.]+@([a-z0-9]([a-z0-9-][a-z0-9])?\.)+[a-z0-9]([a-z0-9-][a-z0-9])?$/i;
+  const lud16Regex = /^[a-z0-9-_.]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+
   if (!lud16Regex.test(lud16)) {
     throw new Error("Invalid lud16 format");
   }
